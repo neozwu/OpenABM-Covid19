2aa4eab0302d4d041e38a36302aa1cc29e440658
Matthew Abueg <mattea@users.noreply.github.com>
2020-07-21 01:35:36 -0700

    add school reopening and excluding app user MT simulations

--- examples/generate_simulations.py
+++ examples/generate_simulations.py
@@ -31,14 +31,34 @@ manual_tracing_nwork = [(f"en_{rate:0.1f}_man_trace_numwork_{nwork}", {
     "app_turned_on": 1,
     "manual_trace_on": 1,
     "app_users_fraction": rate,
-    "manual_trace_n_workers": nwork,
-   #  "manual_trace_n_workers_per_100k": nwork,
-#}) for rate, nwork in itertools.product([0,0.2,0.4,0.6,0.8,1], [4.7, 8.3, 15, 30, 100])]
-}) for rate, nwork in itertools.product([0,0.2,0.4,0.6,0.8,1], [0, 22, 49, 405, 810])]
+   # "manual_trace_n_workers": nwork,
+    "manual_trace_n_workers_per_100k": nwork,
+}) for rate, nwork in itertools.product([0,0.2,0.4,0.6,0.8,1], [0, 4.7, 8.3, 15, 30, 100])]
+# }) for rate, nwork in itertools.product([0,0.2,0.4,0.6,0.8,1], [0, 22, 49, 405, 810])]
 #Totals for WA State are 1371,1500,2285,1000000
 
+school_manual_tracing_nwork = [(f"en_{rate:0.1f}_school_man_trace_numwork_{nwork}", {
+    "app_turned_on": 1,
+    "manual_trace_on": 1,
+    "app_users_fraction": rate,
+    "predicted_lockdown_occupation_multiplier_primary_network": 0.8,
+    "predicted_lockdown_occupation_multiplier_secondary_network": 0.8,
+    "manual_trace_n_workers_per_100k": nwork,
+}) for rate, nwork in itertools.product([0, 0.4], [0, 4.7, 8.3, 15, 30, 100])]
+
+exclude_app_manual_tracing_nwork = [(f"en_{rate:0.1f}_exclude_app_man_trace_numwork_{nwork}", {
+    "app_turned_on": 1,
+    "manual_trace_on": 1,
+    "app_users_fraction": rate,
+    "manual_trace_exclude_app_users": 1,
+    "manual_trace_n_workers_per_100k": nwork,
+}) for rate, nwork in itertools.product([0, 0.4, 0.8], [0, 4.7, 8.3, 15, 30, 100])]
+
 def main():
-  all_sweeps = list(itertools.chain(adoption_sweep, social_distancing, test_wait, manual_tracing_delay, manual_tracing_nwork))
+  all_sweeps = list(itertools.chain(adoption_sweep, social_distancing, test_wait,
+                                    manual_tracing_delay, manual_tracing_nwork,
+                                    school_manual_tracing_nwork,
+                                    exclude_app_manual_tracing_nwork))
   sims = []
   for sim_name, params in all_sweeps:
     for idx in range(10):
